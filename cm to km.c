6c219f1bee2ffdb7cebd61e78334aa0d0dc1f41e
tejav123 <95008577+tejav123@users.noreply.github.com>
2021-11-25 10:07:20 +0530

    Create cm to km.c

---
+++ cm to km.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float cm,km;
+	int b=100000;
+	printf("the value in cm");
+	scanf("%f",&cm);
+	km= cm/b;
+	printf("the value of km for cm is %f",km);
+	return 0;
+	
+}
